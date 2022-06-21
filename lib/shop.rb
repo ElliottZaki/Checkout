2dba297912d018c3752da7f8be27aa902687be7a
Elliott Zaki <elliottzaki@Elliotts-Air.broadband>
2022-06-21 15:09:25 +0100

    Discount method applied. Tests pass

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,4 +1,12 @@
+require 'set'
+
 class Shop
+
+  attr_reader :total
+
+  def initialize
+    @total = 0
+  end
 
 inventory = Hash.new  
 ["A"].each {|item| inventory[item] = 50}
@@ -6,13 +14,75 @@ inventory = Hash.new
 ["C"].each {|item| inventory[item] = 20}
 ["D"].each {|item| inventory[item] = 15}
 
-selected_items = "AAB"
+# selected_items = [["AAB"], ["AAA"], ["ABCD"], ["AA"], ["AAAAAA"]]
+selected_items = ("AAB")
+
 
   def checkout(selected_items, inventory)
-    total = 0
-    p selected_items
-    selected_items.upcase.split('').each {|item| total += inventory[item]}
-      return total
+    basket = selected_items.split('')
+    basket.each {|item| @total += inventory[item]}
+      p @total
+      p basket
+      discount_01(basket)
+      p @total
+      return @total
   end
+
+  def discount_01(basket)
+    amount = basket.count("A")
+    sqrt = Math.sqrt(amount).floor
+    if amount % 3 == 0
+      @total -= ((20) * sqrt)
+    end
+  end
+
+
+  # for every multiple of 3 apply a reduction 
+
+
+  # def subset?(basket)
+  #   p basket
+  #   basket.to_s.each == "A"
+  #   return @total - 20
+  # end
+
+  # def discount_01(basket, total)
+  #   p basket
+  #   basket.uniq.any? do |x| 
+  #     if basket.count(x) % 3 == 0 && x == "A"
+  #       @total - 20
+  #       p @total
+  #     end
+  #   end 
+  # end
+
+
+  
+  # def discount_01(basket, total)
+  #   p basket
+  #   basket.each do |item| 
+  #     if item == "A" && item % 3 == 0
+  #       @total -= 20
+  #     end
+  #   end
+  # end
+
+
+  # def checkout(selected_items, inventory)
+  #   @total = 0
+  #   basket = selected_items.split('')
+  #   p basket
+  #   basket.each {|item| total += @INVENTORY[item]}
+  #   discount_01(basket)
+  #   return total
+  # end
+
+  # def discount_01(basket)
+  #   basket.each do |item| 
+  #     if item == "A" && item % 3 == 0
+  #       @total - 20
+  #     end
+  #   end
+  # end
 
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -3,8 +3,12 @@ require './lib/shop.rb'
 describe Shop do
 
   let (:shop) { Shop.new }
-  let (:inventory) {{"A"=>50, "B"=>30, "C"=>20, "D"=>15}}
-  let (:selected_items) {"AAB"}
+  let (:shop_inventory) {{"A"=>50, "B"=>30, "C"=>20, "D"=>15}}
+  # let (:basket) {[["AAB"], ["AAA"], ["ABCD"], ["AA"], ["AAAAAA"]]}
+  let (:basket) {"AAA"}
+  let (:basket2) {"AAB"}
+  let (:basket3) {"AAAAAA"}
+  let (:basket4) {"AAAAAAAAA"}
 
   it 'Creates a new shop object' do
     expect(shop).to eq (shop)
@@ -12,9 +16,50 @@ describe Shop do
 
     describe '#checkout' do
       it 'takes a string and returns an integer or total price' do
-        expect(shop.checkout(selected_items, inventory)).to eq (130)
+        # expect(shop.checkout(basket2, shop_inventory)).to eq (130)
+        # expect(shop.checkout(basket[3], shop_inventory)).to eq (100)
+        # expect(shop.checkout(basket[2], shop_inventory)).to eq (115)
       end
-    end 
+    end
+    
+    describe '#discount_01' do
+      it 'reduces total price by £20 for every 3x "A" items purchased' do
+        expect(shop.checkout(basket, shop_inventory)).to eq (130)
+      end
+
+      it 'reduces total price by £40 for 6x "A" items purchased' do
+        expect(shop.checkout(basket3, shop_inventory)).to eq (260)
+      end
+
+      it 'reduces total price by £40 for 6x "A" items purchased' do
+        expect(shop.checkout(basket4, shop_inventory)).to eq (390)
+      end
+    end
+
+
+    # let (:shop) { Shop.new }
+    # let (:shop_inventory) {{"A"=>50, "B"=>30, "C"=>20, "D"=>15}}
+    # # let (:basket) {[["AAB"], ["AAA"], ["ABCD"], ["AA"], ["AAAAAA"]]}
+    # let (:basket) {"AAB"}
+    # let (:basket_02) {"AAA"}
+  
+    # it 'Creates a new shop object' do
+    #   expect(shop).to eq (shop)
+    # end
+  
+    #   describe '#checkout' do
+    #     it 'takes a string and returns an integer or total price' do
+    #       expect(shop.checkout(basket, shop_inventory, shop.total)).to eq (130)
+    #       # expect(shop.checkout(basket[3], shop_inventory)).to eq (100)
+    #     end
+    #   end
+      
+    #   describe '#discount_01' do
+    #     it 'reduces total price by £20 for every 3x "A" purchased.' do
+    #       expect(shop.checkout(basket_02, shop_inventory, shop.total)).to eq (130)
+    #       # expect(shop.checkout(basket[4], shop_inventory)).to eq (260)
+    #     end
+    #   end
 
 
 end
